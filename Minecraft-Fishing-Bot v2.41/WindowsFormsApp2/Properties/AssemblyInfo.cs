4c666ac0e5fa677d7e6fe7e5edfe35c4bdb5d434
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2023-11-30 18:24:51 -0600

    Update AssemblyInfo.cs

--- Minecraft-Fishing-Bot v2.41/WindowsFormsApp2/Properties/AssemblyInfo.cs
+++ Minecraft-Fishing-Bot v2.41/WindowsFormsApp2/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Minecraft Auto-Fishing Bot v2.40")]
+[assembly: AssemblyTitle("Minecraft Auto-Fishing Bot v2.41")]
 [assembly: AssemblyDescription("For Farming Those Fish; Day In, Day Out.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RuSS Tools Inc.")]
